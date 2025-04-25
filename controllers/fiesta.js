dba85c8389eb56faa578175719680ac79eee5f7d
Oscar Ramirez <ing.oarrs@gmail.com>
2025-04-24 22:45:26 -0600

    dsa

--- controllers/fiesta.js
+++ controllers/fiesta.js
@@ -322,7 +322,7 @@ const getFiestasBySalon = async (req, res = response) => {
 }
 const changeStatus = async (req, res = response) => {
 
-  let today = new Date().getTime() + 604800000
+  let today = new Date().getTime() + 60480000000
   var desactivadas = []
   try {
     const fiestaDB = await Fiesta.find()
